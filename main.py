7dbe0207ebbdf78c9dc531569822c5f1c8da2796
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-23 16:58:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def welcome(message):
 def handle_text(message):  
     if message.text == "➕ Добавить в базу":
         if message.from_user.id == ADMIN:
-            message.chat.id in in_admin: adminka.in_adminka(message.chat.id, message.text, message.chat.username)
+            message.chat.id in in_admin: adminka.in_adminka(message.chat.id, message.text, message.from_user.id)
 
     if message.text == "⚙️ Инструменты":  
         keyboard = types.InlineKeyboardMarkup()
