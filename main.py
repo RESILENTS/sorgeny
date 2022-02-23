41854aeec65c4f7e3bf348ca4314388e2f570ce8
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-23 23:21:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def podcategors(call):
 		bot.delete_message(chat_id=call.message.chat.id,message_id=call.message.message_id)
 		main = telebot.types.ReplyKeyboardMarkup(True)
 		bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="Готово")
-		bot.send_message(idasd,eply_markup=main)
+		bot.send_message(idasd,reply_markup=main)
 
 
 def callback_inline(call):
