c5ee418c9438fd370674327e0a9ba651af290202
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-24 09:04:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,8 @@ def podcategors(call):
 		main = telebot.types.ReplyKeyboardMarkup(True)
 		bot.answer_callback_query(callback_query_id=call.id, show_alert=True, call_data=create_db, text="Готово")
 		bot.send_message(idasd,reply_markup=main)
-        if call.data == "create_db":
+
+if call.data == "create_db":
                 bot.register_next_step_handler(call.message, create_db1)
 ​
 def​ ​create_db​1(​id, message​): 
