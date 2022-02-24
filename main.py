2183363f9a911f9fbe8e134162c5a74788c3d535
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-24 07:01:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def podcategors(call):
 		bot.send_message(idasd,reply_markup=main)
 
 if call.data == 'create_db':
-bot​.​send_message​(​chat_id​=​id​, ​text​=​'Введите id пользователя кому вы хотите выдать администратора'​) 
+bot​.​send_message​(​chat_id​=​call​, ​text​=​'Введите id пользователя кому вы хотите выдать администратора'​) 
 ​bot​.​register_next_step_handler​(​call​.​message​, ​create_db1)
 
 ​def​ ​create_db​1(​id, message​): 
