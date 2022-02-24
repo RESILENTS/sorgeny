7793c268424e21238bffa52b7b8e3b0932acfcb5
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-24 09:26:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,6 @@ from config import token
 bot=telebot.TeleBot(token)
 ADMIN = 641892529
 idcanal = 1001418408821
-conn = sqlite3.connect('db.db', check_same_thread=False)
-cursor = conn.cursor()
-
-def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
-	cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
-	conn.commit()
 
 kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
 kb.add(types.InlineKeyboardButton(text="➕ Добавить в базу"))
