ff526415e182c84505513ed3daa89d538fdb8309
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-24 20:23:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def add3(message):
 {m2}''',parse_mode='HTML',reply_markup=keyboard)
 
 def db_table_val(link_id: int, link_coment: str, link_text: str):
-    cursor.execute(f'INSERT INTO links (link_id, link_coment, link_text) VALUES ({m1}, {m3}, {m2})')
+    cursor.execute(f'''INSERT INTO links (link_id, link_coment, link_text) VALUES ({m1}, {m3}, {m2})''')
     conn.commit()
 
 @bot.callback_query_handler(func=lambda call:True)
