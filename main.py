dd12a357454d957db257b205568d353a6bf81ee3
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-25 07:05:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ cursor = conn.cursor()
 @bot.message_handler(commands=["start"])
 def welcome(message):
     global go_to_menu
+    go_to_menu = callback_data="go_to_menu"
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_main1 = types.KeyboardButton(text="📩 Получить хайд")
     btn_main2 = types.KeyboardButton(text="📤 Новый запрос")
     keyboard.add(btn_main1, btn_main2)
-    bot.send_message(message.chat.id, "🏠 Меню", reply_markup=keyboard, callback_data="go_to_menu")
+    bot.send_message(message.chat.id, "🏠 Меню", reply_markup=keyboard, go_to_menu)
 
     userid = str(message.chat.id)
 
