2cd8e780e48bb77f17c3818c4e5753b453c3a8fd
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-25 11:30:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def welcome(message):
     text = "SORGENY — Я помогу тебе получить скрытую информацию с разных интернет ресурсов.\n\nУ меня есть база данных слитых хайдов с разных интернет площадок. Более подробнее о боте вы сможете узнать в разделе информация."
     img = open ('welc.webp', 'rb')
     keyboard = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text="📩 Получить хайд", callback_data="test")
+    btn1 = types.InlineKeyboardButton(text="📩 Получить хайд", callback_data="getlink2")
     btn2 = types.InlineKeyboardButton(text="ℹ️ Информация", callback_data="test")
     btn3 = types.InlineKeyboardButton(text="💭 Наш чат", callback_data="test")
     btn4 = types.InlineKeyboardButton(text="📢 Наш канал", callback_data="test")
@@ -126,8 +126,10 @@ def podcategors(call):
         bot.register_next_step_handler(msg, add1)
 
     if call.data == 'getlink2':
-        msg = bot.send_message(call.message.chat.id, '➕ Введите главную ссылку.\n\n Внимание! По этой ссылке будет производится поиск в базе данных.',parse_mode='HTML')
-        bot.register_next_step_handler(msg, get_link2, exists)
+        global link_idm
+        link_idm = message.text
+        msg = bot.send_message(message.chat.id, '➕ Введите ссылку для поиска в базе данных',parse_mode='HTML')
+        bot.register_next_step_handler(msg, getlinkm)
 
     if call.data[:14] == 'принятьзаявку_':
         idasd = call.data[14:]
