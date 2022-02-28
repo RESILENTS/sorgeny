7fab39e8b3ee683fddb810f40a62bbfd0976e16f
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-28 22:05:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ def podcategors(call):
         db_table_val(link_id=link_id, link_coment=link_coment, link_text=link_text)
 
     if call.data == 'get_new':
-        global get_link_new, link_id
+        global get_link_new
         get_link_new = link_id
         db_get_new(new_link=get_link_new)
         
