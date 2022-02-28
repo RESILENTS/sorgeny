6c622857b9fc86e980815ed1f56670cbc4faa718
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-02-28 21:32:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,16 +20,13 @@ def db_table_val(user_id, username):
 
 @bot.message_handler(commands=["start"])
 def welcome(message):
-    global user_id, username, sql, records, num_us
+    global user_id, username, sql, records, result2
     user_id = message.from_user.id
     username = message.from_user.username
 
     sql = f'''INSERT INTO users (user_id, username) VALUES ('{user_id}', '{username}')'''
     result = cursor.fetchall()
-    sqlite_select_query = """SELECT * from users"""
-    cursor.execute(sqlite_select_query)
-    records = cursor.fetchall()
-    bot.send_message(message.chat.id, 'Пользователей:', len(records))
+    cursor.execute("select count(*) from users") result2 = cursor.fetch_one()
 	
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_main1 = types.KeyboardButton(text="📩 Получить хайд")
@@ -37,7 +34,7 @@ def welcome(message):
     btn_main3 = types.KeyboardButton(text="🏠 Главное меню")
     keyboard.add(btn_main1, btn_main2)
     keyboard.add(btn_main3)
-    bot.send_message(message.chat.id, '🏠 Добро пожаловать в главное меню. Пользователей: {num_us}',parse_mode='HTML', reply_markup=keyboard)
+    bot.send_message(message.chat.id, '🏠 Добро пожаловать в главное меню. Пользователей: {result2}',parse_mode='HTML', reply_markup=keyboard)
 
     userid = str(message.chat.id)
     text = '<b>SORGENY</b> — Я помогу тебе получить скрытую информацию с разных интернет ресурсов.\n\nУ меня есть база данных слитых хайдов с разных интернет площадок. Более подробнее о боте вы сможете узнать в разделе информация.'
